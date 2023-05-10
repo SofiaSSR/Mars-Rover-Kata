bfb55d7d64667c294c6db11b5590494c9569016a
SofiaSSR <60446009+SofiaSSR@users.noreply.github.com>
2023-05-10 08:41:05 -0500

    crash to border handle

--- marsRover.js
+++ marsRover.js
@@ -1,23 +1,27 @@
-function parseInput(input){
-    input.split('\n')
-    return {
-    grid:{
-        width: input[0].split(' ')[0] ,
-        height: input[0].split(' ')[1]
-    }
-    };
+function getGridFromInput(input){
+    return newGrid(input.split('\n')[0].split(' ')[0] ,input.split('\n')[0].split(' ')[1]);
 }
 function hasRoverCrashToGridBorder(rover,grid){
-    return rover.x > -1 && rover.x <= grid.width && rover.y > -1 && rover.y <= grid.height
+    return rover.x <0 || rover.x > grid.width || rover.y < 0 || rover.y > grid.height
 }
 
+function roverDoesNotCrashVerification(rover,grid){
+    if(hasRoverCrashToGridBorder(rover,grid)){
+        throw new Error('Rover has crashed to grid border!');
+    }
+}
 function newRover(x,y,orientation){
     return {
         x: x,
         y: y,
         orientation: orientation
     }
-
+}
+function newGrid(width,height){
+    return {
+        width: width ,
+        height: height
+    }
 }
 
 function getUsableOrientation(orientation){
@@ -28,9 +32,11 @@ function getUsableOrientation(orientation){
         'E' :[1,0]
     }[orientation]
 }
-function getFinalCoordinates(rover, instructions){
+function getFinalCoordinates(rover, instructions,grid){
     return instructions.split('').reduce(
-        (currentRover,instruction)=>calculateNextCoordinate(currentRover,instruction),
+        (currentRover,instruction)=>{
+            roverDoesNotCrashVerification(currentRover,grid)
+            return calculateNextCoordinate(currentRover,instruction)},
         rover)
 }
 function calculateNextCoordinate(rover,instruction){
@@ -80,11 +86,9 @@ function moveFoward(rover){
 function calculateMovingFoward(axis,magnitude,orientation){
     return magnitude+getUsableOrientation(orientation)[axis==='x'? 0 :1]
 }
-function parseRovertResult(rover){
+function parseRoverResult(rover){
     return Object.values(rover).join(' ')
 }
-
-
 
 module.exports = {
     turnToRight,
@@ -92,6 +96,7 @@ module.exports = {
     moveFoward,
     getFinalCoordinates,
     calculateNextCoordinate,
-    newRover
+    newRover,
+    newGrid
 }
 
--- test.js
+++ test.js
@@ -1,35 +1,57 @@
 const marsRover = require('./marsRover.js')
+const {describe} = require("node:test");
 
-test(" basic turn left", async function() {
-    expect(marsRover.turnToLeft(marsRover.newRover(0,0,'N'))).toStrictEqual(marsRover.newRover(0,0,'W'));
-    expect(marsRover.turnToLeft(marsRover.newRover(0,0,'W'))).toStrictEqual(marsRover.newRover(0,0,'S'));
-    expect(marsRover.turnToLeft(marsRover.newRover(0,0,'S'))).toStrictEqual(marsRover.newRover(0,0,'E'));
-    expect(marsRover.turnToLeft(marsRover.newRover(0,0,'E'))).toStrictEqual(marsRover.newRover(0,0,'N'));
+describe("basic turn right",()=>{
+    test("makes orientation N to E", async function() {
+        expect(marsRover.turnToRight(marsRover.newRover(0,0,'N'))).toStrictEqual(marsRover.newRover(0,0,'E'));
+    });
+    test("makes orientation S to W", async function() {
+        expect(marsRover.turnToLeft(marsRover.newRover(0,0,'W'))).toStrictEqual(marsRover.newRover(0,0,'S'));
+    });
 });
 
-test(" basic turn right", async function() {
-    expect(marsRover.turnToRight(marsRover.newRover(0,0,'N'))).toStrictEqual(marsRover.newRover(0,0,'E'));
-    expect(marsRover.turnToRight(marsRover.newRover(0,0,'E'))).toStrictEqual(marsRover.newRover(0,0,'S'));
-    expect(marsRover.turnToRight(marsRover.newRover(0,0,'S'))).toStrictEqual(marsRover.newRover(0,0,'W'));
-    expect(marsRover.turnToRight(marsRover.newRover(0,0,'W'))).toStrictEqual(marsRover.newRover(0,0,'N'));
+describe("basic move foward",()=>{
+    test("moves rovert in 1 1 N to 1 2 N", async function() {
+        expect(marsRover.moveFoward(marsRover.newRover(1,1,'N'))).toStrictEqual(marsRover.newRover(1,2,'N'));
+    });
+    test("moves rovert in 1 1 E to 2 1 N", async function() {
+        expect(marsRover.moveFoward(marsRover.newRover(1,1,'E'))).toStrictEqual(marsRover.newRover(2,1,'E'));
+    });
+    test("moves rovert in 1 1 S to 1 0 N", async function() {
+        expect(marsRover.moveFoward(marsRover.newRover(1,1,'S'))).toStrictEqual(marsRover.newRover(1,0,'S'));
+    });
+    test("moves rovert in 1 1 W to 0 1 N", async function() {
+        expect(marsRover.moveFoward(marsRover.newRover(1,1,'W'))).toStrictEqual(marsRover.newRover(0,1,'W'));
+    });
 });
 
-test(" basic move foward", async function() {
-    expect(marsRover.moveFoward(marsRover.newRover(1,1,'N'))).toStrictEqual(marsRover.newRover(1,2,'N'));
-    expect(marsRover.moveFoward(marsRover.newRover(1,1,'E'))).toStrictEqual(marsRover.newRover(2,1,'E'));
-    expect(marsRover.moveFoward(marsRover.newRover(1,1,'S'))).toStrictEqual(marsRover.newRover(1,0,'S'));
-    expect(marsRover.moveFoward(marsRover.newRover(1,1,'W'))).toStrictEqual(marsRover.newRover(0,1,'W'));
+describe("calculate rover's next coordinate",()=>{
+    test("instruction R makes rovert in 1 1 N to 1 1 E", async function() {
+        expect(marsRover.calculateNextCoordinate(marsRover.newRover(1,1,'N'),'R')).toStrictEqual(marsRover.newRover(1,1,'E'));
+    });
+    test("instruction M makes rovert in 1 1 E to 2 1 E", async function() {
+        expect(marsRover.calculateNextCoordinate(marsRover.newRover(1,1,'E'),'M')).toStrictEqual(marsRover.newRover(2,1,'E'));
+    });
+    test("instruction L makes rovert in 1 1 S to 1 1 E", async function() {
+        expect(marsRover.calculateNextCoordinate(marsRover.newRover(1,1,'S'),'L')).toStrictEqual(marsRover.newRover(1,1,'E'));
+    });
+    test("instruction M makes rovert in 1 1 W to 0 1 W", async function() {
+        expect(marsRover.calculateNextCoordinate(marsRover.newRover(1,1,'W'),'M')).toStrictEqual(marsRover.newRover(0,1,'W'));
+    });
 });
-
-test("calculate rover's next coordinate", async function() {
-    expect(marsRover.calculateNextCoordinate(marsRover.newRover(1,1,'N'),'R')).toStrictEqual(marsRover.newRover(1,1,'E'));
-    expect(marsRover.calculateNextCoordinate(marsRover.newRover(1,1,'E'),'M')).toStrictEqual(marsRover.newRover(2,1,'E'));
-    expect(marsRover.calculateNextCoordinate(marsRover.newRover(1,1,'S'),'L')).toStrictEqual(marsRover.newRover(1,1,'E'));
-    expect(marsRover.calculateNextCoordinate(marsRover.newRover(1,1,'W'),'M')).toStrictEqual(marsRover.newRover(0,1,'W'));
-});
-
-test("calculate rover's final coordinate ", async function() {
-    expect(marsRover.getFinalCoordinates(marsRover.newRover(1,2,'N'),'LMLMLMLMM')).toStrictEqual(marsRover.newRover(1,3,'N'));
-    expect(marsRover.getFinalCoordinates(marsRover.newRover(3,3,'E'),'MMRMMRMRRM')).toStrictEqual(marsRover.newRover(5,1,'E'));
+describe("calculate rover's final coordinate ", async function() {
+    test("for first given input :", function (){
+        expect(marsRover.getFinalCoordinates(marsRover.newRover(1,2,'N'),'LMLMLMLMM',marsRover.newGrid(5,5))).toStrictEqual(marsRover.newRover(1,3,'N'));
+    });
+    test("for second given input :", function (){
+        expect(marsRover.getFinalCoordinates(marsRover.newRover(3,3,'E'),'MMRMMRMRRM',marsRover.newGrid(5,5))).toStrictEqual(marsRover.newRover(5,1,'E'));
+    });
+    test("for crash option :", function (){
+        try{marsRover.getFinalCoordinates(marsRover.newRover(3,3,'E'),'MMMM',marsRover.newGrid(5,5))
+        }
+        catch(e){
+            expect(e).toEqual(new Error('Rover has crashed to grid border!'))
+        };
+    });
 });
 
